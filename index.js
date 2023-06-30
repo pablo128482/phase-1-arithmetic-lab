6a7ba05d9d6affe2970175c1bb802ae5ae0c9b7d
pablo128482 <ashley.c.clay@gmail.com>
2023-06-30 10:45:07 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 
-// Write your code here
+// let multiply = function(number) {
+//     number = num1
+//     number2 = num2
+//     // Multipy with a result of 62 within an equation
+//     let num1 = 2;
+//     let num2 = 31;
+//     let result = num1 *= num2; //=> 62
+//     console.log(result);
+//     };
+
+let num1 = 2;
+let num2 = 31;
+let multiply = (num1 * num2)
+console.log(multiply)
+
+let random = Math.floor(Math.random() * 0) + 1;
+console.log(random)
+//     // Generate a random integer greater than 0
+//     Math.floor(Math.random() * 0) + 1;
+//     //=> 8
+//     }
+ let num3 = 40;
+ let num4 = 10;
+ let remainder = 4;
+ let mod  = (num3 / num4)
+ console.log(remainder)
+
+ let max = Math.max(20)
+ const numbers = [1, 5, 20, 10, 8]
+ console.log (max)
